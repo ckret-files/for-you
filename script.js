eaf4b653fc39cfdaa19a4ab54f540bc6f6950ab6
ckret-files <eljhonpaulocolumna.loterte@bicol-u.edu.ph>
2026-05-28 21:40:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -214,7 +214,7 @@ function startSequence() {
                   setChatButtons([
                     {
                       id: "btn-accept",
-                      text: "Accept ♥",
+                      text: "Suree Why Not",
                       type: "primary",
                       handler: () =>
                         sendDecisionSequence(
@@ -225,7 +225,7 @@ function startSequence() {
                     },
                     {
                       id: "btn-decline",
-                      text: "Decline",
+                      text: "Thank you for the invite but, I'll have to decline your offer",
                       type: "secondary",
                       handler: () =>
                         sendDecisionSequence(
